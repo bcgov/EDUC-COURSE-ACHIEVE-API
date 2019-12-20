49f00f6e5c3ed77143f1bf1003d61476308e3bc6
Michael Bonar <michael.w.bonar@gmail.com>
2019-12-20 09:21:43 -0800

    Initial Commit

---
+++ src/main/java/ca/bc/gov/educ/api/courseachievement/courseachievementapi/CourseAchievementApiApplication.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.educ.api.courseachievement.courseachievementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAchievementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAchievementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/bc/gov/educ/api/courseachievement/courseachievementapi/CourseAchievementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.educ.api.courseachievement.courseachievementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAchievementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
